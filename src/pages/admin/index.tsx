86752bc072bafb22b8d6e1b77eb7a9678f9284ae
Luan Rafael <luanraffaell@hotmail.com>
2025-04-14 07:10:04 -0300

    Projeto criado

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,7 @@
+export function Admin(){
+    return(
+        <div>
+            <h1>Página admin</h1>
+        </div>
+    )
+}
---
+++ src/pages/networks/index.tsx
@@ -0,0 +1,7 @@
+export function Networks(){
+    return(
+        <div>
+            <h1>Página Redes sociais</h1>
+        </div>
+    )
+}
