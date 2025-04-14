0f8597cbaa69e382c87065288038bded87486532
Luan Rafael <luanraffaell@hotmail.com>
2025-04-14 08:59:41 -0300

    feat: home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,33 @@
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+import { Social } from "../../components/social";
+
 export function Home(){
     return(
-        <div className="w-full h-screen bg-neutral-950">
-            <h1 className="text-white text-3xl" >Página Home</h1>
+        <div className="flex flex-col w-full py-4 items-center justify-center">
+            <h1 className="md:text-4xl  text-3xl font-bold text-white mt-20">Página Home</h1>
+            <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
+
+            <main className="flex flex-col w-11/12 max-w-xl text-center">
+                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+                    <a>
+                        <p className="text-base md:text-lg">
+                            Canal no Youtube
+                        </p>
+                    </a>
+                </section>
+
+                <footer className="flex justify-center gap-3 my-4">
+                    <Social url="http://facebook.com/luanraffaell">
+                        <FaFacebook size={35} color="#fff" />
+                    </Social>
+                    <Social url="https://www.youtube.com/@luanrafaeldacruz">
+                        <FaYoutube size={35} color="#fff" />
+                    </Social>
+                    <Social url="http://instagram.com/luanraffaell">
+                        <FaInstagram size={35} color="#fff" />
+                    </Social>
+                </footer>
+            </main>
         </div>
     )
 }
